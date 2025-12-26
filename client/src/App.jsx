872e268e0808a721e72f53f7fc157a4c25b8e23a
INDRA-011 <indr.yznn.77@gmail.com>
2025-12-26 15:54:31 +0545

    dashboard removed due to vibe code unmatched

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Landing from "./pages/Landing/Landing";
+import AdminDashboard from "./pages/Admin-Dashboard/admindashboard";
 
 const App = () => {
-  return <Landing />;
+  return <Landing />
+  ;
+  
 };
-
+<AdminDashboard/>
 export default App;
