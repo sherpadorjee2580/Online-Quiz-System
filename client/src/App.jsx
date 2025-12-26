bac59a10880df4890b45e1973d3c5c84fc8dfb45
INDRA-011 <indr.yznn.77@gmail.com>
2025-12-26 22:51:45 +0545

    AdminDashboard added successfully

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
 import Landing from "./pages/Landing/Landing";
-import AdminDashboard from "./pages/Admin-Dashboard/admindashboard";
+import AdminDashboard from "./pages/AdminDashboard/AdminDashboard";
+
 
 const App = () => {
-  return <Landing />
-  ;
+  return (
+    <>
+  <Routes>
+    <Route path="/" element={<Landing />} />
+    <Route path="/admin" element={<AdminDashboard />} />
+    
+  </Routes>
+  </>
+  );
   
 };
-<AdminDashboard/>
 export default App;
